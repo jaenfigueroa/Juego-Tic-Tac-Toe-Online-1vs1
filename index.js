dca2d0f975dafaaf6b9d7f240e1c2ff4e450aa89
Jaen Figueroa <69079292+jaenfigueroa@users.noreply.github.com>
2022-10-22 16:57:15 -0500

    detectar que ya tiene pareja terminado

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-// const { json } = require('express')
 const express = require('express')
 const servidor = express()
 
@@ -10,144 +9,209 @@ const cors = require('cors')
 servidor.use(cors())
 
 //para generar id unicos
-// const { v4: uuidv4 } = require('uuid')
+const { v4: uuidv4 } = require('uuid')
 
 /////////////////////////////////////////////////////////////
-// let listaEspera = []
-// let listaParejas = []
-
 // class Jugador {
-//   constructor(id, nombre, pieza) {
+//   constructor(id, nombre) {
 //     this.id = id
 //     this.nombre = nombre
-//     this.pieza = pieza
 //   }
 // }
 
-//////////////////////////////////////////////////////////////
-//REGISTRAR JUGADOR NUEVO/////////////////////////////////////
-// servidor.post('/unirme/:nombre', (req, res) => {
-
-//   let nombreJugador = req.params.nombre
-//   let idJugador = uuidv4()
-
-//   let jugadorNuevo = new Jugador(nombreJugador, idJugador)
-
-//   listaEspera.push(jugadorNuevo)
-
-//   if (listaEspera.length === 2) {
-
-//     let contenido = {
-
-//     }
-
-//     res.send(true)
-//   } else {
-//     res.send(false)
+// class Pareja {
+//   constructor(jugador1, jugador2, indexGrupo, tablero) {
+//     this.jugador1 = jugador1
+//     this.jugador2 = jugador2
+//     this.indexGrupo = indexGrupo
+//     this.tablero = tablero
 //   }
+// }
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+let tablero = ['', '', '', '', '', '', '', '', '']
+let jugadoresEnEspera = []
+let jugadoresConPareja = []
 
-// })
+// REGISTRAR JUGADOR NUEVO/////////////////////////////////////
+servidor.get('/unirme/:nombre', (req, res) => {
 
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-let jugadoresEspera = []
+  let id = uuidv4()
+  let nombre = req.params.nombre
 
-let jugadoresParejas = [
-  [
-    jugadores = [
-      {
-        "id": 11111,
-        "nombre": 'Mario',
-        "permiso": true
-      },
-      {
-        "id": 22222,
-        "nombre": 'Sofia',
-        "permiso": false
-      }
-    ],
-    tablero = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
-  ],
-  [
-    jugadores = [
-      {
-        "id": 33333,
-        "nombre": 'Jaen',
-        "permiso": true
-      },
-      {
-        "id": 44444,
-        "nombre": 'Ahini',
-        "permiso": false
-      }
-    ],
-    tablero = ['X', 'X', 'X', 'X', 'X', 'X', 'X', 'X', 'X']
+  // let jugadorNuevo = new Jugador(id, nombre)
+
+  let jugadorNuevo = [
+    id,
+    nombre
   ]
-]
 
+  console.log('jugador nuveo', jugadorNuevo);
+  //agregar el jugador a la lista de espera
+  jugadoresEnEspera.push(jugadorNuevo)
 
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////
-///PARA RECIBIR TABLERO ACTUALIZADO//////////////////////////
+  console.log('Jugador agregado a la lista de espera');
+  console.log(jugadoresEnEspera);
 
-servidor.post('/enviarTablero/:indiceUsuario/:indiceGrupo', (req, res) => {
-  const indiceGrupo = req.params.indiceGrupo
-  // const indiceUsuario = req.params.indiceUsuario
-  const tableroActualizado = req.body.contenido
+  if (jugadoresEnEspera.length >= 2) {
 
-  let grupoPareja = jugadoresParejas[indiceGrupo]
-  grupoPareja[1] = tableroActualizado //ono
+    let indexGrupo = jugadoresConPareja.length
 
-  let jugador0 = grupoPareja[0][0]
-  let jugador1 = grupoPareja[0][1]
+    let nuevaPareja = [
+      [
+        jugadoresEnEspera[0],
+        jugadoresEnEspera[1]
+      ],
+      indexGrupo,
+      tablero
+    ]
 
-  jugador0.permiso = !jugador0.permiso
-  jugador1.permiso = !jugador1.permiso
+    jugadoresConPareja.push(nuevaPareja)
+    jugadoresEnEspera = []
+  }
+
+  console.log(jugadoresConPareja);
 
   res.json({
-    candado: [jugador0.permiso, jugador1.permiso]
+    mensaje: 'usuario agregado con exito',
+    id
   })
 })
 
+// VEREFICAR SI TIENE PAREJA////////////////////////////////
+servidor.post('/verificarPareja/:id', (req, res) => {
+  let id = req.params.id
 
-///PARA ENVIAR TABLERO ACTUALIZADO////////////////////////////
-servidor.get('/traerTablero/:id/:indiceGrupo', (req, res) => {
-  let indiceGrupo = req.params.indiceGrupo
-  // let idUsuario = req.params.idUsuario
+  console.log('indice que llega', id);
 
-  const grupoPareja = jugadoresParejas[indiceGrupo][1]
+  console.log('vista previa', jugadoresConPareja);
 
-  console.log(grupoPareja);
-  console.log(typeof grupoPareja);
+  if (jugadoresConPareja.length > 0) {
 
-  res.json({
-    array: grupoPareja
-  })
+    let indiceDelGrupo = buscarindexGrupo(jugadoresConPareja, id)
+
+    console.log('inidce del grupo', indiceDelGrupo);
+
+    if (indiceDelGrupo !== -1) {
+
+      res.json({
+        seEncontro: true,
+        indiceDelGrupo,
+        indexJugador: 10000,
+        arrayCompleto: 20000,
+      })
+    } else {
+      res.json({
+        seEncontro: false
+      })
+    }
+
+  } else {
+    res.json({
+      seEncontro: false
+    })
+  }
+
 })
+
 
 /////////////////////////////////////////////////////////////
-//ESPERAR TURNO//////////////////////////////////////////////
-servidor.get('/esperarTurno/:indiceGrupo/', (req, res) => {
-  const indiceGrupo = req.params.indiceGrupo
+function buscarindexGrupo(array, id) {
 
-  let grupoPareja = jugadoresParejas[indiceGrupo]
+  for (let x = 0; x < array.length; x++) {
 
-  let jugador0 = grupoPareja[0][0]
-  let jugador1 = grupoPareja[0][1]
+    for (let y = 0; y < 2; y++) {
 
-  res.json({
-    candado: [jugador0.permiso, jugador1.permiso],
-    // tablero: jugadoresParejas[1][1]
-  })
+      for (let z = 0; z < 2; z++) {
 
-})
+        if (array[x][y][z][0] == id) {
+          console.log('se encontro el index del grupo es: ' + x);
+          return x
+        }
 
+      }
+    }
+  }
+  return -1
+}
+/////////////////////////////////////////////////////////////
 /////////////////////////////////////////////////////////////
 const PUERTO = process.env.PORT || 3000
 
 servidor.listen(PUERTO, () => {
   console.log(`Servidor escuchando en el puerto: ${PUERTO}`);
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////
+///PARA RECIBIR TABLERO ACTUALIZADO//////////////////////////
+
+// servidor.post('/enviarTablero/:indiceUsuario/:indiceGrupo', (req, res) => {
+//   const indiceGrupo = req.params.indiceGrupo
+//   // const indiceUsuario = req.params.indiceUsuario
+//   const tableroActualizado = req.body.contenido
+
+//   let grupoPareja = jugadoresParejas[indiceGrupo]
+//   grupoPareja[1] = tableroActualizado //ono
+
+//   let jugador0 = grupoPareja[0][0]
+//   let jugador1 = grupoPareja[0][1]
+
+//   jugador0.permiso = !jugador0.permiso
+//   jugador1.permiso = !jugador1.permiso
+
+//   res.json({
+//     candado: [jugador0.permiso, jugador1.permiso]
+//   })
+// })
+
+
+///PARA ENVIAR TABLERO ACTUALIZADO////////////////////////////
+// servidor.get('/traerTablero/:id/:indiceGrupo', (req, res) => {
+//   let indiceGrupo = req.params.indiceGrupo
+//   // let idUsuario = req.params.idUsuario
+
+//   const grupoPareja = jugadoresParejas[indiceGrupo][1]
+
+//   console.log(grupoPareja);
+//   console.log(typeof grupoPareja);
+
+//   res.json({
+//     array: grupoPareja
+//   })
+// })
+
+/////////////////////////////////////////////////////////////
+//ESPERAR TURNO//////////////////////////////////////////////
+// servidor.get('/esperarTurno/:indiceGrupo/', (req, res) => {
+//   const indiceGrupo = req.params.indiceGrupo
+
+//   let grupoPareja = jugadoresParejas[indiceGrupo]
+
+//   let jugador0 = grupoPareja[0][0]
+//   let jugador1 = grupoPareja[0][1]
+
+//   res.json({
+//     candado: [jugador0.permiso, jugador1.permiso],
+//     // tablero: jugadoresParejas[1][1]
+//   })
+
+// })
+
